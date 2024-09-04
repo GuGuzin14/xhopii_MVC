b18bcc02335e0a507cefc54509be3bfcd40bff41
Gustavo <gustavobispocosta5521@gmail.com>
2024-09-04 13:09:18 -0300

    Funcionario Gustavo

--- controller/Controlador.php
+++ controller/Controlador.php
@@ -2,6 +2,7 @@
 
 require_once "../model/BancoDeDados.php";
 require_once "../model/Produto.php";
+require_once "../model/Funcionario.php";
 class Controlador{
 
     private $bancoDeDados;
@@ -30,6 +31,27 @@ class Controlador{
             "<p>Fabricante:" . $produto["fabricante"] . "</p>".
             "<p>Descrição:" . $produto["descricao"] . "</p>".
             "<p>Valor:" . $produto["valor"] . "</p>".
+            "</section>";
+
+        }
+    }
+
+    public function inserirFuncionario($cpf,$nome,$sobrenome,$dataNasc,$telefone,$email,$salario){
+        $funcionario = new Funcionario($cpf,$nome,$sobrenome,$dataNasc,$telefone,$email,$salario);
+        $this->bancoDeDados->inserirFuncionario($cpf,$nome,$sobrenome,$dataNasc,$telefone,$email,$salario);
+    }
+
+    public function visualizarFuncionarios(){
+        $listaFuncionarios = $this->bancoDeDados->retornarFuncionarios();
+        while($funcionario = mysqli_fetch_assoc($listaFuncionarios)){
+            return "<section class =\"conteudo-bloco\">".
+            "<h2>". $funcionario["nome"] . "</h2>".
+            "<p>Sobrenome:" . $funcionario["sobrenome"] . "</p>".
+            "<p>Cpf:" . $funcionario["cpf"] . "</p>".
+            "<p>Data de Nascimento:" . $funcionario["dataNasc"] . "</p>".
+            "<p>Telefone:" . $funcionario["telefone"] . "</p>".
+            "<p>Email:" . $funcionario["email"] . "</p>".
+            "<p>Salario:" . $funcionario["salario"] . "</p>".
             "</section>";
 
         }
